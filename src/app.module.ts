797cd09981ec5af8301301c917f674178e291510
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-09-27 11:09:28 -0300

    database initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+// import configuration from './config/database.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/alias-game'), UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
