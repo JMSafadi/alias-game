9f1145ce20173a396296bab39a943c6a5ccccda2
Marlena Duleba <marlena_duleba@o2.pl>
2024-09-28 22:47:26 +0200

    set up eslint jest plugin
    clean up unnecessary characters using npm run format

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'jest'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:jest/recommended',
   ],
   root: true,
   env: {
@@ -21,8 +22,8 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
     'no-unused-vars': 'warn',
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'jest/consistent-test-it': ['error', { fn: 'it' }],
     'jest/no-focused-tests': 'error',
     'no-eval': 'error',
