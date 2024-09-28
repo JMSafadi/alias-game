17ee95d9230db12fe86796846d1a4e6751cb5d7c
Marlena Duleba <marlena_duleba@o2.pl>
2024-09-28 19:51:38 +0200

    update ESLint rules for Type Script and code quality

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,14 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    'no-unused-vars': 'warn',
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'jest/consistent-test-it': ['error', { fn: 'it' }],
+    'jest/no-focused-tests': 'error',
+    'no-eval': 'error',
   },
 };
