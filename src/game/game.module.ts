69adf1f57fc1960a1adf818d84d50f85045bd5fb
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-10-01 13:58:46 -0300

    /game module

---
+++ src/game/game.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Game, GameSchema } from './schemas/Game.schema';
+import { GameService } from './game.service';
+import { GameController } from './game.controller';
+
+@Module({
+  imports: [MongooseModule.forFeature([
+    {
+      name: Game.name,
+      schema: GameSchema
+    }
+  ])],
+  providers: [GameService],
+  controllers: [GameController]
+})
+
+export class GameModule { }
