c2ef9688e08ba442745ca1d48b16b03750329eea
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-10-01 13:52:36 -0300

    delete test user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), GameModule],
   controllers: [],
   providers: [],
 })
