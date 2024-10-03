01cef685de73c7e956bbc2fa7cb4950f3d917d9a
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-10-03 00:23:26 -0300

    start game and turn/guess word -  socket events

--- src/game/gateways/game.gateway.ts
+++ src/game/gateways/game.gateway.ts
@@ -2,12 +2,14 @@ import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect,
 import { Server, Socket } from 'socket.io'
 import { StartGameDto } from "../dto/start-game.dto";
 import { GameService } from "../game.service";
-import { StartTurnDto } from "../dto/start-turn.dto";
+import { GuessWordDto } from "../dto/guess-word.dto";
 
 @WebSocketGateway({ cors: true })
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server
+
+  private activeTimers = new Map()
 
   handleConnection(client: any, ...args: any[]) {
     console.log(`Client connected: ${client.id}`)
@@ -22,14 +24,57 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage('startGame')
   async handleStartGame(@MessageBody() startGameDto: StartGameDto, @ConnectedSocket() client: Socket) {
     const game = await this.gameService.startGame(startGameDto)
-    console.log(game)
     this.server.emit('gameStarted', { message: 'Game started!', game })
+    // Init new game state
+    const updatedGame = await this.gameService.startTurn({
+      gameId: game._id.toString(),
+      teamName: game.teamsInfo[0].teamName,
+    })
+    // Emit start turn event
+    console.log('Updated game state: ', updatedGame)
+
+    this.server.emit('turnStarted', {
+      message: `Turn started for team: ${updatedGame.currentTurn.teamName}`,
+      wordToGuess: updatedGame.currentTurn.wordToGuess
+    })
+    // Start timeout for first turn
+    this.startTurnTimer(updatedGame.id, updatedGame.currentTurn.teamName, updatedGame.timePerTurn)
   }
-  // Event to init turn
-  @SubscribeMessage('startTurn')
-  async handleStartTurn(@MessageBody() StartTurnDto: StartTurnDto, @ConnectedSocket() client: Socket) {
-    const turn = await this.gameService.startTurn(StartTurnDto)
-    console.log(turn)
-    this.server.emit('turnStarted', { message: 'Turn started', turn } )
+
+  // Method to init timer
+  private startTurnTimer(gameId: string, teamName: string, timePerTurn: number) {
+    if (this.activeTimers.has(gameId)) {
+      clearTimeout(this.activeTimers.get(gameId))
+    }
+    const timer = setTimeout(async () => {
+      console.log(`Turn ended by timeout for team: ${teamName}`)
+
+      const turn = await this.gameService.endTurn(gameId)
+      this.server.emit('turnEnded', { message: 'Turn ended due to timeout', turn })
+
+      const nextTurn = await this.gameService.startNextTurn(gameId)
+      this.server.emit('turnStarted', nextTurn)
+      this.startTurnTimer(gameId, nextTurn.currentTurn.teamName, nextTurn.timePerTurn)
+
+    }, timePerTurn * 1000)
+    this.activeTimers.set(gameId, timer)
+  }
+  @SubscribeMessage('guessWord')
+  async handleGuessWord(@MessageBody() guessWordDto: GuessWordDto, @ConnectedSocket() client: Socket) {
+    const { gameId, teamName, guessWord } = guessWordDto
+  // Verify guess attempt
+    const result = await this.gameService.checkWordGuess(gameId, teamName, guessWord)
+    // If correct, end turn
+    if (result.correct) {
+      const turnEnded = await this.gameService.endTurn(gameId)
+      this.server.emit('turnEnded', { message: 'Correct word guessed! Turn ended', turn: turnEnded })
+      // Jump next turn
+      const nextTurn = await this.gameService.startNextTurn(gameId)
+      this.server.emit('turnStarted', nextTurn)
+      this.startTurnTimer(gameId, nextTurn.currentTurn.teamName, nextTurn.timePerTurn)
+    } else {
+      // If incorrect word, send notify
+      client.emit('guessFailed', { message: 'Incorrect word! Try again.' })
+    }
   }
 }
