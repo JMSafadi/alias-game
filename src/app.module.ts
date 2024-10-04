8ed08f30a13d2b0414972b16d317e4ed833e76c1
Agustin Tafura <atafura@the-8agency.com>
2024-10-04 18:51:06 -0300

    login and signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule.forRoot(), // Asegúrate de que esto esté presente
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { User } from "src/schemas/User.schema";
-import { CreateUserDto } from "./dto/CreateUser.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/schemas/user.schema';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
     createUser(createUserDto: CreateUserDto) {
-        const newUser = new this.userModel(createUserDto)
-        return newUser.save()
+        const newUser = new this.userModel(createUserDto);
+        return newUser.save();
     }
 
     getUsers() {
-        return this.userModel.find()
+        return this.userModel.find();
     }
 
     getUserById(id: string) {
-        return this.userModel.findById(id)
+        return this.userModel.findById(id);
     }
 }
