fa41b914cdcc1ef710904128c770dde122bb769c
Agustin Tafura <atafura@the-8agency.com>
2024-10-12 13:19:17 -0300

    app docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 /**
  * AppController provides endpoints for retrieving general information about the application.
  */
+@ApiTags('App') // Categoría del controlador en Swagger
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   /**
    * GET /
@@ -14,6 +16,7 @@ export class AppController {
    * @returns A string message indicating the server status.
    */
   @Get()
+  @ApiOperation({ summary: 'Get welcome message' }) // Resumen de la operación
   getHello(): string {
     return this.appService.getHello();
   }
