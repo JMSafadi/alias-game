207cb7f28ee1d8f4b0fc56da8f2d6157d8a56fde
Agustin Tafura <atafura@the-8agency.com>
2024-10-12 13:15:12 -0300

    lobby docs

--- src/lobby/dto/CreateLobby.dto.ts
+++ src/lobby/dto/CreateLobby.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsString, IsInt, Min, Max } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateLobbyDto {
-    @IsNotEmpty({ message: 'This fild is required.'})
-    @IsString()
-    userId: string;
+  @ApiProperty({
+    description: 'ID of the user creating the lobby.',
+    example: 'user123',
+  })
+  @IsNotEmpty({ message: 'This field is required.' })
+  @IsString()
+  userId: string;
 
+  @ApiProperty({
+    description: 'Number of players per team. Min: 2, Max: 5.',
+    example: 3,
+  })
   @IsInt()
   @Min(2, { message: 'The minimum number of players is 2.' })
   @Max(5, { message: 'The maximum number of players is 5.' })
